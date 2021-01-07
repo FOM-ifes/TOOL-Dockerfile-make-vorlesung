d83a0136a0dc92983d7ce44016f854750bc17e2f
Norman Markgraf <nmarkgraf@hotmail.com>
2021-01-07 12:57:43 +0100

    Erster Aufschlag

---
+++ dockerfile
@@ -0,0 +1,43 @@
+FROM rocker/r-ver:4.0.3
+
+ENV S6_VERSION=v2.1.0.2
+ENV RSTUDIO_VERSION=latest
+ENV PATH=/usr/lib/rstudio-server/bin:$PATH
+
+# RUN /rocker_scripts/install_rstudio.sh
+RUN /rocker_scripts/install_pandoc.sh
+RUN /rocker_scripts/install_tidyverse.sh
+ENV CTAN_REPO=http://mirror.ctan.org/systems/texlive/tlnet
+ENV PATH=/usr/local/texlive/bin/x86_64-linux:$PATH
+RUN /rocker_scripts/install_verse.sh
+
+RUN apt-get update && \
+    apt-get -y install python3-pip tcl tk expect \
+    && pip3 install panflute
+RUN apt-get clean
+RUN install2.r --error \
+    mosaic \
+    tidyverse \
+    rmarkdown \
+    knitr \
+    tinytex \
+    git2r \
+    reticulate \
+    plot3D \
+    rgl \
+    futile.logger \
+    formatR \
+    futile.options \
+    wordcloud \
+    lambda.r \
+    latex2exp \
+    optparse \
+    getopt 
+
+RUN mkdir /home/Vorlesungen
+RUN mkdir /home/Vorlesungen/results
+WORKDIR /home/Vorlesungen
+
+COPY make-docker.R /home/Vorlesungen
+
+ENTRYPOINT Rscript make-docker.R
---
+++ make-docker.R
@@ -0,0 +1,97 @@
+#!/usr/bin/env Rscript
+# R Skript
+
+# Paket zum Umgang mit GitHub Repositoies
+library(git2r)
+
+# Paket zum Parsen von Parametern
+library(optparse)
+
+option_list = list(
+    make_option(c("-u", "--username"),
+                type="character",
+                default=NULL,
+                help="Benutzername GitHub",
+                metavar="character"),
+    make_option(c("-p", "--password"),
+                type="character",
+                default=NULL,
+                help="Passwort für den GitHub Account",
+                metavar="character"),
+    make_option(c("-r", "--repourl"),
+                type="character",
+                default="https://github.com/NMarkgraf/MathGrundDer-W-Info",
+                help="URL zum Repository [default= %default]",
+                metavar="character")
+    make_option(c("-n", "--name"),
+                            type="character",
+                            default="Vorlesungen",
+                            help="Name des Repository [default= %default]",
+                            metavar="character")
+);
+
+opt_parser <- OptionParser(option_list=option_list);
+opt <- parse_args(opt_parser);
+
+# default:
+username <- NULL
+password <- NULL
+if (!is.null(opt$username)) {
+  username <- opt$username
+}
+if (!is.null(opt$username)) {
+  password <- opt$password
+}
+
+
+# Setzen der Zugangsdaten (falls nötig/möglich)
+if (!is.null(username)) {
+  if (!is.null(passwort)) {
+    cred <- cred_user_pass(username = username, password = password)
+  } else {
+    cred <- cred_user_pass(username = username)
+  }
+} else {
+  cred <- NULL
+}
+
+repo_name <- opt$Name
+repo_url <- opt$repourl
+
+# Lade das Repository "Vorlesungen" von GitHub
+
+## Create a temporary directory to hold the repository
+# path <- file.path(tempfile(pattern="git2r-"), repo_name)
+
+# Verzeichnis anlegen in welches das Ropsiory geclont wird:
+path <- file.path("/home/Vorlesungen", repo_name)
+dir.create(path, recursive=TRUE)
+
+
+## Clone the git2r repository
+repo <- clone(repo_url, path, credentials=cred)
+
+# In das geklonte Verzeichnis wechseln
+setwd(path)
+
+# Zuerst das "RunMeFirst.R" starten, damit alle weiteren Pakete
+# installiert werden
+source("RunMeFirst.R")
+
+# Den eigentlichen render-Prozess starten:
+source("makerender.R")
+
+# Ergebnisse in "results" kopieren
+
+dir_list <- list.files(".", ".pdf$")
+
+results_path <- "/home/Vorlesungen/results"
+
+for(pdf in dir_list) {
+  file.copy(from = pdf,
+            to = results_path,
+            overwrite = TRUE,
+            recursive = FALSE,
+            copy.mode = TRUE,
+            copy.date = TRUE)
+}
